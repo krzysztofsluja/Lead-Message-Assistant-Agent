1e55ba15075432225feef0f5b6968090d02ec2be
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-12-26 15:18:37 +0100

    Init commit

---
+++ src/test/java/com/sluja/lma/assistant/AssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sluja.lma.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
