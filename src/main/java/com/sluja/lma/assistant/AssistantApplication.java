1f27a3cb4bf0595331ba244dcffb8b2396543e86
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-12-29 21:04:45 +0100

    Enhance application configuration by enabling transaction management and updating JPA settings.

--- src/main/java/com/sluja/lma/assistant/AssistantApplication.java
+++ src/main/java/com/sluja/lma/assistant/AssistantApplication.java
@@ -3,9 +3,11 @@ package com.sluja.lma.assistant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAsync
+@EnableTransactionManagement
 public class AssistantApplication {
 
 	public static void main(String[] args) {
