83d77e97d9841e5255d545ae9ce287ea5c2ed054
krzysztofsluja <slujakrzysztof6@gmail.com>
2024-12-29 21:21:36 +0100

    Enhance NewOperatorDataRequestDTO with custom toString method - hide password field

--- src/main/java/com/sluja/lma/assistant/operator/processes/signup/dto/request/NewOperatorDataRequestDTO.java
+++ src/main/java/com/sluja/lma/assistant/operator/processes/signup/dto/request/NewOperatorDataRequestDTO.java
@@ -3,9 +3,19 @@ package com.sluja.lma.assistant.operator.processes.signup.dto.request;
 import java.io.Serializable;
 
 public record NewOperatorDataRequestDTO(String firstName,
-        String lastName,
-        String username,
-        String password,
-        String email) implements Serializable {
+                String lastName,
+                String username,
+                String password,
+                String email) implements Serializable {
+
+        @Override
+        public String toString() {
+                return "NewOperatorDataRequestDTO{" +
+                                "firstName='" + firstName + '\'' +
+                                ", lastName='" + lastName + '\'' +
+                                ", username='" + username + '\'' +
+                                ", email='" + email + '\'' +
+                                '}';
+        }
 
 }
